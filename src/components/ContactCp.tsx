0dcc75ae424fbfd00c3d71aaa52d016c0ccd1863
Delassi Othmane <od@papershift.com>
2021-07-31 06:04:26 +0200

    create contact component

---
+++ src/components/ContactCp.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import {Contact} from "../App";
+import {Button} from "react-bootstrap";
+
+
+const ContactCp: React.FC<Contact> =  ({contact}) => {
+    return (
+        <div className="contact">
+            <div className="contact-info">
+                <span>{contact.name}</span>
+                <span>{contact.phone}</span>
+            </div>
+            <div className="contact-action">
+                <Button className="button" variant="success" onClick={(e) => e.stopPropagation()}>
+                    Call
+                </Button>
+            </div>
+        </div>
+    )
+}
+
+export default ContactCp
